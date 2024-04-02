b9c94a20f96344d36f8de4ec25b9f65661ecf067
Ayush Chinmay <ayush.chinmaya@gmail.com>
2024-04-02 07:05:04 -0400

    Dynamic sleep

--- TTS/tts_test1.py
+++ TTS/tts_test1.py
@@ -1,10 +1,13 @@
 from pygame import mixer
-import time
+from time import sleep
 from datetime import datetime
 
-mixer.init()
+# Constants
+sleeper = 1
+lastHour12 = 0
 
-time12 = {0:'12AM', 1:'1AM', 2:'2AM', 3:'3AM', 4:'4AM', 5:'5AM', 6:'6AM', 7:'7AM', 8:'8AM', 9:'9AM', 10:'10AM', 11:'11AM', 12:'12PM', 13:'1PM', 14:'2PM', 15:'3PM', 16:'4PM', 17:'5PM', 18:'6PM', 19:'7PM', 20:'8PM', 21:'9PM', 22:'10PM', 23:'11PM'}
+# Variables
+mixer.init()
 
 def hourly_voice_reminder():
 	"""
@@ -17,18 +20,29 @@ def hourly_voice_reminder():
 	Returns:
 		None
 	"""
+	global sleeper, lastHour12
+	lastHour12 = int(datetime.now().strftime("%I"))
+
 	while True:
 		now = datetime.now()
-		currHour = int(now.strftime("%H"))
-		currTime = now.strftime("%M:%S")
-		fileName = f"/Audio/{time12[currHour]}.mp3"
+		currHour12 = int(now.strftime("%I"))
+		ampm = now.strftime("%p")
+		currSec = int(now.strftime("%S"))
+		
+		# File name format: <hour><AM/PM>.mp3
+		fileName = f"/Audio/{currHour12}{ampm}.mp3"
 
-		if currTime == "00:00":
+		if currSec == 0:
+			sleeper = 60
+
+		if currHour12 != lastHour12:
+			sleeper = 600
+			lastHour12 = currHour12
 			mixer.music.load(fileName)
 			mixer.music.play()
 			print("<HOURLY VOICE REMINDER>")
 
-		print(datetime.now().strftime("%H:%M:%S"))
-		time.sleep(1)
+		print(f"{now.strftime('%r')}")
+		sleep(sleeper)
 
 hourly_voice_reminder()
